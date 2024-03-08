dbec97678e30aade0ee1efef816022228d256dbb
Dogan Yildirim <dgnyldrmcoc@gmail.com>
2024-03-08 23:28:31 +0300

    Proje dosyası ekle.

---
+++ MiniLoginWithIdentityLibrary/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiniLoginWithIdentityLibrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
